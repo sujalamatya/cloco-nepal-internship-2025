b0f53a84226dad7759523768796e5bbba8ad3393
sujalamatya <sujalcr10@gmail.com>
2025-02-25 10:09:26 +0545

    style:update css of voting website

--- 2025-02-22/nuqs-practice/src/app/layout.tsx
+++ 2025-02-22/nuqs-practice/src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
-import { type ReactNode } from "react";
+import "./globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
