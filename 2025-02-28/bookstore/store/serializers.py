12a45f5953357bf6608b6982196877f3e57fcd66
sujalamatya <sujalcr10@gmail.com>
2025-03-03 09:40:18 +0545

    fix: change Category serializer

--- 2025-02-28/bookstore/store/serializers.py
+++ 2025-02-28/bookstore/store/serializers.py
@@ -15,10 +15,17 @@ class AuthorSerializer(serializers.Serializer):
         instance.save()
         return instance
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Category
-        fields='__all__'
+class CategorySerializer(serializers.Serializer):
+    id=serializers.IntegerField(read_only=True)
+    name= serializers.CharField(max_length=50)
+
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
+    
+    def update(self,instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
 
 class PublisherSerializer(serializers.ModelSerializer):
     class Meta:
